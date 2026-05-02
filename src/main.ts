5e2eb3976480a23f9d1b184f67fc6455201d6709
Arteks718 <artexbro.official@gmail.com>
2026-05-02 19:12:12 +0300

    feat: added using CORS and validation for incoming data

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS for external frontend requests
+  app.enableCors();
+  
+  // Set up global ValidationPipe to automatically validate incoming data
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
